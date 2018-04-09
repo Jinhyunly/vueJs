f3df72926af1af6d5e33ac43ef08235ec0d6d2ea
Jinhyunly <dbdbddipp@gmail.com>
2018-04-10 03:30:31 +0900

    recommend click ok

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,5 +1,5 @@
 import SearchModel from './models/SearchModel.js'
-
+import KeywordModel from './models/KeywordModel.js'
 new Vue({
     el: '#app' ,
     data: {
@@ -7,10 +7,12 @@ new Vue({
         submitted: false,
         tabs: ['추천 검색어', '최근 검색어'],
         selectedTab: '',
+        keywords: [],
         searchResult: []
     },
     created() {
         this.selectedTab = this.tabs[0]
+        this.fetchKeyword()
     },
     methods: {
         onSubmit(e){
@@ -24,6 +26,15 @@ new Vue({
         },
         onClickTab(tab){
             this.selectedTab = tab
+        },
+        onClickKeyword(keyword){
+            this.query = keyword  //클릭된 item이 입력값으로 바인딩된다
+            this.search()
+        },
+        fetchKeyword(){
+            KeywordModel.list().then(data => {
+                this.keywords = data
+            })
         },
         search(){
             SearchModel.list().then(data => {
