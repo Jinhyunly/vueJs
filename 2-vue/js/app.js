fa484d60980d0b8fd5c31b0b1a05190f9c142173
Jinhyunly <dbdbddipp@gmail.com>
2018-04-10 02:31:47 +0900

    resultForm 1,2 ok

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,17 +1,29 @@
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
     el: '#app' ,
     data: {
         query: '',
-        searchResert: []
+        submitted: false,
+        searchResult: []
     },
     methods: {
         onSubmit(e){
-            
+            this.search()
         },
-        onKeyup(){
+        onKeyup(e){
             if(!this.query.length) this.resetForm()
         },
-        onReset(){
+        onReset(e){
+            this.resetForm()
+        },
+        search(){
+            SearchModel.list().then(data => {
+                this.submitted = true
+                this.searchResult = data
+            })
+        },
+        resetForm(){
             this.query = '' //this => vue instance
             // todo 검색결과를 숨기는..
             debugger
