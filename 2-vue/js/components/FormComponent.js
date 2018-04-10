5459104d8e93da8df8e3691945093ed6e32f5c1a
Jinhyunly <dbdbddipp@gmail.com>
2018-04-11 02:15:41 +0900

    add FormComponent start

---
+++ 2-vue/js/components/FormComponent.js
@@ -0,0 +1,19 @@
+export default {
+    template: '#search-form',
+    data() {
+        return {
+            query : ''
+        }
+    },
+    method : {
+        onSubmit(){
+
+        },
+        onKeyup(){
+
+        },
+        onReset(){
+
+        }
+    }
+}
